c951f120a3a7201f2e97f57235b53bae9ee13c71
manasbekoff <manasbekov@gmail.com>
2020-03-21 17:39:35 +0600

    new

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,79 +1,70 @@
 package com.company;
 
+import java.awt.List;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
-        ArrayList<String> languages = new ArrayList<>();
+        ArrayList<String> ListA = new ArrayList<>();
         Scanner scan = new Scanner(System.in);
-        System.out.println("Please type languages that will be divided into A and B");
+        System.out.println("Please type ListA that will be divided into A and B");
         TimeUnit.SECONDS.sleep(1);
         System.out.println("Please, first type a list A:");
-        languages.add(scan.next());
-        languages.add(scan.next());
-        languages.add(scan.next());
-        languages.add(scan.next());
-        languages.add(scan.next());
-        //System.out.println("List A: " + languages);
-        for (String language : languages) {
-            System.out.println(language);
+        ListA.add(scan.next());
+        ListA.add(scan.next());
+        ListA.add(scan.next());
+        ListA.add(scan.next());
+        ListA.add(scan.next());
+        Iterator<String> iteratorA = ListA.iterator();
+        while (iteratorA.hasNext()) {
+            System.out.println(iteratorA.next());
         }
         TimeUnit.SECONDS.sleep(1);
-        ArrayList<String> languages2 = new ArrayList<>();
+        ArrayList<String> ListB = new ArrayList<>();
         System.out.println("Please type a list B");
-        languages2.add(scan.next());
-        languages2.add(scan.next());
-        languages2.add(scan.next());
-        languages2.add(scan.next());
-        languages2.add(scan.next());
-        for (String s : languages2) {
-            System.out.println(s);
+        ListB.add(scan.next());
+        ListB.add(scan.next());
+        ListB.add(scan.next());
+        ListB.add(scan.next());
+        ListB.add(scan.next());
+        Iterator<String> iteratorB = ListB.iterator();
+        while (iteratorB.hasNext()) {
+            System.out.println(iteratorB.next());
         }
-        languages.addAll(languages2);
-        //languages.addAll(languages2);
+        //ListA.addAll(ListB);
 
-        ArrayList<String> languages3 = new ArrayList<>(languages);
-        Collections.sort(languages);
-        System.out.println("Sort" + languages3);
-        Collections.reverse(languages);
-        //printList(languages2);
-        //printList(languages3);
-        //Collections.sort(languages);
-        //Collections.reverse(languages2);
-        //while (lang.hasNext())
-        Iterator<String> iterator3 = languages.iterator();
-        for (int i = 0; i < languages.size(); i++) {
-            //languages.addAll(languages2);
-            //languages3.addAll(languages);
-            }
-            //Iterator<String> lang3 = languages3.iterator3();
-            while (iterator3.hasNext()) {
-                System.out.println(iterator3.next());
-            }
+        ArrayList<String> ListC = new ArrayList<>();
+        System.out.println("List C:");
+        Collections.reverse(ListB);
+        //Iterator<String> iteratorC = ListC.iterator();
+        for (int i = 0; i < ListA.size(); i++) {
+            ListC.add(ListA.get(i));
+            ListC.add(ListB.get(i));
+            //Iterator<String> iterator = ListC.iterator();
+            //Collections.addAll(ListC);
+            System.out.println(ListC.get(i));
+
         }
+        //while (iteratorC.hasNext()) {
+            //System.out.println(iteratorC.next());
+            //while (iteratorC.hasNext()) {
+            //System.out.println(s);
+            //System.out.println(iteratorC.next());
 
-        //  System.out.println(lang.next());
+            System.out.println("Sorted:");
+            ListC.sort(Comparator.comparing(String::length));
+            ListC.forEach(System.out::println);
 
-        private static void printList (ArrayList < ? > list){
-            for (Object a : list) {
-                System.out.println(a);
-            }
+            //Comparator<String> byLength = new Comparator<String>() {
+            //          @Override
+            //        public int compare(String a1, String a2) {
+            //          return a1.length() - a2.length();
+            //    }
+            //};
+            //Collections.sort(ListC, byLength);
+            //System.out.println("Sorted");
+            //System.out.println(ListC);
         }
     }
-
-
-
-
-    //private static void printList1(ArrayList<?> list) {
-      //  for (int i = 0; i < list.size(); i++) {
-        //    System.out.println("A" + i + " - " + list.get(i));
-        //}
-    //}
-
-    //private static void printList2(ArrayList<?> list) {
-      ///  for (int i = 5; i < list.size(); i--) {
-    ///System.out.println("B" + i + " - " + list.get(i));
-       /// }
-    ///}
